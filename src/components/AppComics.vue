183f6e304ff0654193546a1ab348939db58550ce
roberto.boston <roberto.bozzo97@tiscali.it>
2023-01-13 17:12:35 +0100

    props done

---
+++ src/components/AppComics.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        comic : Object
+    }
+    
+}
+</script>
+
+<template lang="">
+    <div>
+        <div class="comics-card">
+            <div class="thumb-container">
+                <img :src="comic.thumb" class="thumb" :alt="comic.series">
+            </div>
+            <div class="title-container">
+                 <h4>{{comic.series}}</h4>
+                 <h4>{{comic.price}}</h4>
+                 <h4>{{comic.type}}</h4>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,12 @@
 <script>
 import comicsData from '../assets/data/datacomics.js';
+import AppComics from './AppComics.vue'
 
 export default {
+    components :{
+        AppComics
+
+    },
     data(){
         return {
             comics: comicsData 
@@ -12,13 +17,57 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-
+    <div class="dark-bg">
+            <div class="title-section">
+                <h2 class="title">Comics Series</h2>
+            </div>
+        <div class="section-fumetti">
+            <div class="comics-container">
+                <AppComics v-for="(item, index) in comics" :key="index" :comic="item"></AppComics>
+            </div>
+        </div>
     </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+.dark-bg{
+    background-color: rgb(33, 33, 33);
+}
+
+
+.title-section{
+    position: relative;
+
+}
+
+.title{
+    text-transform: uppercase;
+    color: white;
+    position: absolute;
+    top: -25px;
+    background-color: $primary;
+    width: 250px;
+    padding: 15px;
+    margin-left: 100px;
+    text-align: center;
+}
+
+.section-fumetti{
+    width: 70%;
+    margin: 0 auto;
+    display: flex
+}
+
+.comics-container{
+    width: 100%;
+    @include flex-v-center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding-top: 5rem
+}
 
 
 
